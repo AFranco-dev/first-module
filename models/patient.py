696c78cff6b38fec2b2764c1ad84a035fd967777
Andre Franco <andre.franco.dev@gmail.com>
2022-12-05 15:37:57 -0600

    module models start vid-3.1

--- models/patient.py
+++ models/patient.py
@@ -5,7 +5,6 @@ from odoo import api, fields, models
 class HospitalPatient(models.Model):
     _name = 'hospital.patient'
     _description = 'Hospital Patient'
-    name = fields.Char(String='Name')
-    age = fields.Integer(String='Age')
+    name = fields.Char(string='Name')
+    age = fields.Integer(string='Age')
     gender = fields.Selection([('male','Male'),('female','Female')], string="Gender")
-    
