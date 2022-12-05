b36677676302df07e1f3cbbea9fd75f50ff4288a
Andre Franco <andre.franco.dev@gmail.com>
2022-12-05 14:23:45 -0600

    Finished manifest basic vid-1

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+# noinspection PyStatementEffect
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'sequence': -100,
+    'summary': 'Hospital management system',
+    'description': "This is the first module",
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
