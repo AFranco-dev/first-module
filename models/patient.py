2208cd768672410d0d8b93af51bdfdb3388cebbe
Andre Franco <andre.franco.dev@gmail.com>
2022-12-06 15:26:49 -0600

    added tracking to name, age and gender vid-18 c1

--- models/patient.py
+++ models/patient.py
@@ -6,8 +6,8 @@ class HospitalPatient(models.Model):
     _name = 'hospital.patient'
     _description = 'Hospital Patient'
     _inherit = ['mail.thread', 'mail.activity.mixin']
-    name = fields.Char(string='Name')
-    age = fields.Integer(string='Age')
-    gender = fields.Selection([('male','Male'),('female','Female')], string="Gender")
+    name = fields.Char(string='Name', tracking='true')
+    age = fields.Integer(string='Age', tracking='true')
+    gender = fields.Selection([('male','Male'),('female','Female')], string="Gender", tracking='true')
     ref = fields.Char(string='Reference')
     active = fields.Boolean(string='Active', default=True)
