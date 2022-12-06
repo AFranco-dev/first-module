0cf9f99974382cc418c6323c5666ee540cc692ae
Andre Franco <andre.franco.dev@gmail.com>
2022-12-06 15:28:28 -0600

    fixed boolean value typo ('') vid-18 c2

--- models/patient.py
+++ models/patient.py
@@ -6,8 +6,8 @@ class HospitalPatient(models.Model):
     _name = 'hospital.patient'
     _description = 'Hospital Patient'
     _inherit = ['mail.thread', 'mail.activity.mixin']
-    name = fields.Char(string='Name', tracking='true')
-    age = fields.Integer(string='Age', tracking='true')
-    gender = fields.Selection([('male','Male'),('female','Female')], string="Gender", tracking='true')
+    name = fields.Char(string='Name', tracking=True)
+    age = fields.Integer(string='Age', tracking=True)
+    gender = fields.Selection([('male','Male'),('female','Female')], string="Gender", tracking=True)
     ref = fields.Char(string='Reference')
     active = fields.Boolean(string='Active', default=True)
