1af083176535b757edcea366137637ad31b486c5
Andre Franco <andre.franco.dev@gmail.com>
2022-12-07 10:40:24 -0600

    fixed date default to context_today vid-22 c2

--- models/appointment.py
+++ models/appointment.py
@@ -9,4 +9,4 @@ class HospitalPatient(models.Model):
 
     patient_id = fields.Many2one(comodel_name='hospital.patient', string="Patient")
     appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
-    booking_date = fields.Date(string='Booking Date', default=fields.Date.now)
+    booking_date = fields.Date(string='Booking Date', default=fields.Date.context_today)
