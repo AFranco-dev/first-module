8ce390c60fdb80470e1c266cc4494f229933cdce
Andre Franco <andre.franco.dev@gmail.com>
2022-12-07 11:03:41 -0600

    fixed selection warning vid-23 c2

--- models/appointment.py
+++ models/appointment.py
@@ -8,6 +8,6 @@ class HospitalPatient(models.Model):
     _description = 'Hospital Appointment'
 
     patient_id = fields.Many2one(comodel_name='hospital.patient', string="Patient")
-    gender = fields.Selection([('male','Male'),('female','Female')], string="Gender", related='patient_id.gender')
+    gender = fields.Selection(related='patient_id.gender')
     appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
     booking_date = fields.Date(string='Booking Date', default=fields.Date.context_today)
