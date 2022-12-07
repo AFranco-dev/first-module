ed6cb4d921e298ae4831f9ff9fa8da094aa597ef
Andre Franco <andre.franco.dev@gmail.com>
2022-12-07 14:14:50 -0600

    added auto ref att vid-25 c2

--- models/appointment.py
+++ models/appointment.py
@@ -12,3 +12,7 @@ class HospitalPatient(models.Model):
     appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
     booking_date = fields.Date(string='Booking Date', default=fields.Date.context_today)
     ref = fields.Char(string='Reference')
+
+    @api.onchange('patient_id')
+    def onchange_patient_id(self):
+        self.ref = self.patient_id.ref
