6b095396a3081e9d9c8d76dcb044ccb912dd2861
Andre Franco <andre.franco.dev@gmail.com>
2022-12-07 11:33:53 -0600

    added computed value vid-24 c1

--- models/patient.py
+++ models/patient.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models
+from datetime import date
 
 
 class HospitalPatient(models.Model):
@@ -7,7 +8,16 @@ class HospitalPatient(models.Model):
     _description = 'Hospital Patient'
     _inherit = ['mail.thread', 'mail.activity.mixin']
     name = fields.Char(string='Name', tracking=True)
-    age = fields.Integer(string='Age', tracking=True)
+    age = fields.Integer(string='Age', compute='_compute_age', tracking=True)
     gender = fields.Selection([('male','Male'),('female','Female')], string="Gender", tracking=True)
     ref = fields.Char(string='Reference')
     active = fields.Boolean(string='Active', default=True)
+    date_of_birth = fields.Date(string="Date Of Birth")
+
+    def _compute_age(self):
+        today = date.today()
+        this_year_birthday = date(today.year, self.date_of_birth.month, self.date_of_birth.day)
+        if this_year_birthday < today:
+            self.age = today.year - self.date_of_birth
+        else:
+            self.age = today.year - self.date_of_birth - 1
